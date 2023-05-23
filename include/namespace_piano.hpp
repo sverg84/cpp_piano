9036677c83ebe445cbf33ed2cb10a0d4f2a65b93
sverg84 <134344913+sverg84@users.noreply.github.com>
2023-05-23 16:25:21 -0700

    add getopt for 'number of keys' CLI argument

--- include/namespace_piano.hpp
+++ include/namespace_piano.hpp
@@ -2,6 +2,7 @@
 #define NAMESPACE_PIANO
 
 namespace piano {
+    const int NUM_KEYS_DEFAULT = 8;
     const int HEIGHT = 1080;
     const int WIDTH = 1920;
 }
--- play.cpp
+++ play.cpp
@@ -1,6 +1,39 @@
+#include <getopt.h>
+#include <iostream>
+#include "include/namespace_piano.hpp"
 #include "include/PianoRunner.hpp"
 
-int main() {
+const option longopts[] {
+    {"num-keys", optional_argument, 0, 'n'},
+    {"help", no_argument, 0, 'h'},
+    {0, 0, 0, 0},
+};
+
+int main(int argc, char** argv) {
+    int numKeys = piano::NUM_KEYS_DEFAULT;
+
+    while (true) {
+        int optionIndex = 0;
+        const int opt = getopt_long(argc, argv, "n:h", longopts, &optionIndex);
+
+        // exit condition for getopt_long
+        if (opt == -1)
+            break;
+
+        // either set argument-specified number of keys
+        // or print help message and exit, or continue
+        switch (opt) {
+            case 'n':
+                numKeys = std::stoi(optarg);
+                break;
+            case 'h':
+                std::cout << "help" << std::endl;
+                return 0;
+            default:
+                break;
+        }
+    }
+
     PianoRunner runner;
     runner.run();
 
